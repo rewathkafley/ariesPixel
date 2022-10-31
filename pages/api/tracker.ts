3caaabdde6f4aa8e536e31a7076ba27eae6cb4a8
t0nylombardi <alombardi.331@gmail.com>
2022-10-31 11:56:56 -0400

    add tailwind to app

--- pages/api/tracker.ts
+++ pages/api/tracker.ts
@@ -7,31 +7,48 @@ import path from 'path'
 const filePath = path.resolve('.', 'tracker/tracker.gif')
 const imageBuffer = fs.readFileSync(filePath)
 
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
   const forwarded = req.headers["x-forwarded-for"]
   const ip = forwarded ? forwarded.split(/, /)[0] : req.connection.remoteAddress
+  const user_agent = req.headers['user-agent']
 
   const {
     query: { campaign, content_type }
   } = req;
 
-  // Create new food
-  const pixel = await prisma.pixel.create({
-    data: {
-      ip_address: ip,
-      campaign: `${req.query.campaign}`,
-      content_type: `${content_type}`,
-      city: '',
-      state: '',
-      user_agent: '',
-    },
+  let headers = new Headers({
+    "Accept"       : "application/json",
+    "Content-Type" : "application/json",
+    "User-Agent"   : "keycdn-tools:https://localhost"
+  });
+
+  let locationData  = await fetch(`https://tools.keycdn.com/geo.json?host=69.118.172.165`, {
+    method  : 'GET', 
+    headers : headers 
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => {
+    return json.data.geo
   });
 
 
-  console.log(pixel, ip);
+  // Create new Pixel
+  await prisma.pixel.create({
+    data: {
+      ip_address: `${locationData.host}`,
+      campaign: `${req.query.campaign}`,
+      content_type: `${content_type}`,
+      city: `${locationData.city}`,
+      state: `${locationData.region_code}`,
+      user_agent: `${user_agent}`,
+    },
+  });
   
   res.setHeader('Content-Type', 'image/jpg')
   res.send(imageBuffer)
--- pages/tracker/index.tsx
+++ pages/tracker/index.tsx
@@ -1,28 +1,64 @@
 import React from 'react'
+import { PrismaClient } from "@prisma/client";
+import { json } from 'stream/consumers';
 
-function Tracker() {
+const prisma = new PrismaClient();
+
+
+const Tracker = ({pixels}:any) => {
   return (
     <div>
-
-          <section className="text-gray-600 body-font">
-          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-                <br className="hidden lg:inline-block" />readymade gluten
-              </h1>
-              <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
-              <div className="flex justify-center">
-                <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-                <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
-              </div>
-            </div>
-            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-              <img className="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
-            </div>
-          </div>
+      <main className="container grid place-items-center">
+        <h1 className="content-center text-6xl font-normal leading-normal mt-0 mb-2">Tracker</h1>
+      </main>
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <table className="table  table-compact table-zebra w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>IP Address</th>
+                <th>Campaign</th>
+                <th>Content Type</th>
+                <th>City</th>
+                <th>State</th>
+                <th>User Agent</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                pixels.map((pixel:any) => {
+                  return (
+                    <tr key={pixel.id}>
+                      <td></td>
+                      <td>{pixel.ip_address}</td>
+                      <td>{pixel.campaign}</td>
+                      <td>{pixel.content_type}</td>
+                      <td>{pixel.city}</td>
+                      <td>{pixel.state}</td>
+                      <td>{pixel.user_agent}</td>
+                    </tr>
+                  )
+                }
+                )
+              }
+            </tbody>
+          </table>
+    
+        </div>
       </section>
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  // Get all foods in the "food" db
+  const pixels = await prisma.pixel.findMany();
+  return {
+    props: {
+      pixels
+    },
+  };
 }
 
 export default Tracker
